e06d1313ec9fd50d902a6e88155accfd89cd9940
Shaelyn <shaelynjoy@gmail.com>
2016-05-14 12:52:24 -0700

    init reactions component per demo plan

--- app/src/components/BaseComponent.jsx
+++ app/src/components/BaseComponent.jsx
@@ -1,10 +1,12 @@
 var React = require('react')
+var ReactionsComponent = require('./ReactionsComponent')
  
 module.exports = React.createClass({
   render: function() {
     return (
       <div>
         Base Component!
+        <ReactionsComponent />
       </div>
     )
   }
