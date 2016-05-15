95b50f1fd43d2f2215c0e3cc2ca0d4e616d9b2a8
Shaelyn <shaelynjoy@gmail.com>
2016-05-15 12:27:40 -0700

    work up to point of implementing sign rxn component

--- app/src/components/ReactionsComponent.jsx
+++ app/src/components/ReactionsComponent.jsx
@@ -11,10 +11,31 @@ export default class ReactionsComponent extends React.Component {
     ],
   };
 
+  constructor(props, context) {
+    super(props, context);
+
+    this.muiDefaultProps = {
+      componentStyle: {
+        marginRight: 'auto',
+        marginLeft: 'auto',
+        padding: '10px',
+        maxWidth: '300px',
+        backgroundColor: 'white',
+        border: '1px solid #81A2CA',
+        borderRadius: '45px',
+      },
+    };
+  }
+
   render() {
+
+  	for (let rxn in this.props.reactions){
+  		console.log('rxn', this.props.reactions[rxn])
+  	}
+
     return (
-      <div>
-        Reactions Component!**
+      <div style={this.muiDefaultProps.componentStyle}>
+        {this.props.reactions[0].rxn}
       </div>
     )
   }
