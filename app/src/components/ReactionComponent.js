a345c8ab175ef7a9105c1287f8df872188b5e81a
Shaelyn <shaelynjoy@gmail.com>
2016-05-17 12:00:45 -0700

    init styling

--- app/src/components/ReactionComponent.js
+++ app/src/components/ReactionComponent.js
@@ -16,21 +16,50 @@ export default class ReactionComponent extends React.Component {
     this.muiDefaultProps = {
       componentStyle: {
         margin: 0,
-        display: 'inline',
+        display: 'inline-block',
+        width: '50px',
       },
       imgStyle: {
         width: '50px',
         height: '50px',
       },
+      labelStyle: {
+        margin: '0',
+        width: '35px',
+        display: 'block',
+      },
+    };
+    this.state = {
+      isExpanded: false,
+      isSelected: false,
     };
   }
 
+  _handleOnHover = () => {
+    //console.log('on hover')
+    this.setState({isExpanded: true})
+  }
+
+  _handleOnHoverLeave = () => {
+    //console.log('on leave')
+    this.setState({isExpanded: false})
+  }
+
   render() {
+    let label;
+    if (this.state.isExpanded){
+      console.log('isExpanded!!!!!')
+      label=(<p style={this.muiDefaultProps.labelStyle}>{this.props.rxnString}</p>);
+    }
+
     return (
       <div style={this.muiDefaultProps.componentStyle}>
+        {label}
         <img
           ref='backgroundImage'
           key={`img-${this.props.rxnString}`}
+          onMouseOver={this._handleOnHover}
+          onMouseLeave={this._handleOnHoverLeave}
           style={this.muiDefaultProps.imgStyle}
           src={this.props.rxnImage}/>
       </div>
--- app/src/components/ReactionsComponent.jsx
+++ app/src/components/ReactionsComponent.jsx
@@ -12,13 +12,13 @@ export default class ReactionsComponent extends React.Component {
 
   constructor(props) {
     super(props);
-
     this.muiDefaultProps = {
       componentStyle: {
         marginRight: 'auto',
         marginLeft: 'auto',
         padding: '10px',
-        maxWidth: '300px',
+        maxWidth: '350px',
+        height: '40px',
         backgroundColor: 'white',
         border: '1px solid #81A2CA',
         borderRadius: '45px',
@@ -28,10 +28,8 @@ export default class ReactionsComponent extends React.Component {
   }
 
   render() {
-
   	let reactionComponents = [];
   	for (let rxn in this.props.reactions){
-  		console.log('rxn', this.props.reactions[rxn])
   		reactionComponents.push(
   			<ReactionComponent 
   				key={`rxncomponent-${this.props.reactions[rxn].rxn}`}
